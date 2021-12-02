c7918ad71b9260282c79617babbd4a9f57755751
Martin Nefjodov <martin.nefjodov@github.com>
2021-12-02 18:38:46 +0000

    correct day 1

--- day-1/main.rb
+++ day-1/main.rb
@@ -3,8 +3,8 @@ lines = File.readlines('./input').map(&:chomp)
 increases = 0
 
 for index in 1..lines.length()-1 do
-    prev = lines[index-1]
-    curr = lines[index]
+    prev = lines[index-1].to_i
+    curr = lines[index].to_i
     if curr > prev
         increases = increases + 1
     end
